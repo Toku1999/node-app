eb0cbef7c7d52d517431eb8ec7922b3b46c5b526
tokesh <tokeshpitchuka100@gmail.com>
2026-04-06 06:45:07 +0000

    code

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("CI/CD Pipeline Running 🚀");
+});
+
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Tokesh" },
+    { id: 2, role: "DevOps Engineer" }
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});

7c755a15c575613086d08129b26390610c713776
Toku1999 <151664279+Toku1999@users.noreply.github.com>
2026-04-16 22:10:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static("public"));
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (MONGO_URI) {
-    mongoose.connect(MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
-} else {
-    console.log("MongoDB not configured, running without DB");
-}
-
-// Routes
-const userRoutes = require("./routes/userRoutes");
-app.use("/api/users", userRoutes);
-
-// Frontend
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.send("App is running 🚀");
 });
 
 app.listen(4000, '0.0.0.0', () => {

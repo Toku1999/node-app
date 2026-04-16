5628b7be97b8e8c616dfd029b23605a40f072f12
Toku1999 <151664279+Toku1999@users.noreply.github.com>
2026-04-16 22:12:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
-
 app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-    res.send("App is running 🚀");
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(4000, '0.0.0.0', () => {
+app.listen(4000, () => {
     console.log("Server running on port 4000");
 });

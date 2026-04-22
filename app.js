826ff377f9be4f4885a14f6c4eb668ddcbbee1ef
Toku1999 <151664279+Toku1999@users.noreply.github.com>
2026-04-22 23:22:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("EKS Fargate Working ✅");
-});
+const MONGO_URI = process.env.MONGO_URI;
 
-// START SERVER FIRST (IMPORTANT)
-app.listen(4000, "0.0.0.0", () => {
-  console.log("Server running on port 4000");
-});
-
-// THEN connect Mongo (optional)
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000");
+});
